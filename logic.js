69a2f35712a4f4a4407d0a65124f2dcccfb9dc65
cubbic <danpopa1998@gmail.com>
2016-07-02 15:29:53 +0300

    Parca terminat :+1:  fara footer

--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $.getJSON('https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json&callback=?',function (data) {
 
       var title = data.query.random[0].title;
-      console.log(link);
+
       var link = 'https://en.wikipedia.org/wiki/' + title;
       window.location.href = link;
     })
@@ -20,7 +20,24 @@ $(document).ready(function(){
 
     function wikipediaHTMLResult(data) {
       console.log(data);
-      
+      var length = data.query.search.length;
+      var searchArr = data.query.search;
+      $('#results').html("");
+      for (var i = 0; i < length; i++) {
+        var title = searchArr[i].title;
+        var snippet = searchArr[i].snippet;
+        var link = 'https://en.wikipedia.org/wiki/' + title;
+
+        InsertResults(title,snippet,link);
+      }
+    }
+    function InsertResults(title,snippet,link) {
+
+      var toAppend = '  <div class="result">' +
+                          '<div class="content">' +
+                            '<a href = "'+ link +'"><h3 class = "content-title">'+title+'</h3></a>'+
+                            '<h4 class="content-text">'+snippet+'</h4>';
+      $('#results').append(toAppend);
     }
   }
 });
