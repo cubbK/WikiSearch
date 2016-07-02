286c329023315a1bcaa5c3ec6faf3ff0b499020d
cubbic <danpopa1998@gmail.com>
2016-07-02 11:25:20 +0300

    returneaza random article

--- logic.js
+++ logic.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   $('h3').html("Hi jquery is working");
+  $.getJSON('http://en.wikipedia.org/w/api.php?action=query&format=json&callback=?',
+  {list:'search' , srsearch: 'paradox', prop:'links', uselang:'en'}, wikipediaHTMLResult);
+
+  function wikipediaHTMLResult(data) {
+    console.log(data);
+    $('h3').html(data.query.search[1].snippet);
+  }
+  $('#random-article').click(redirectRandom);
+  function redirectRandom() {
+    $.getJSON('http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json&callback=?',function (data) {
+
+      var title = data.query.random[0].title;
+      console.log(link);
+      var link = 'https://en.wikipedia.org/wiki/' + title;
+      window.location.href = link;
+    })
+  }
 });
